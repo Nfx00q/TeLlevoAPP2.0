a305e54b9dbc7fbf677c87d4fedbca18795df053
Nfx00q <babgutier@gmail.com>
2024-11-08 01:18:51 -0300

    Huella method purge.

--- src/app/pages/splashscreen/splashscreen.page.ts
+++ src/app/pages/splashscreen/splashscreen.page.ts
@@ -24,48 +24,5 @@ export class SplashscreenPage implements OnInit {
       this.router.navigate(['log-in']);
     }, 2000);
   }
-  async checkLogin() {
-    this.AuthServiceService.isLogged().subscribe(async(user)=>{
-      if(user) {
-        try {
-          // VERIFICAMO CON LA HUELLA
-          await this.checkHuellaDigital();
-  
-          const usuario = await this.firestore.collection('usuarios')
-          .doc(user.uid).get().toPromise();
-          const userData = usuario?.data() as Usuario;
-  
-          if(userData) {
-            if(userData.tipo === 'admin') {
-              this.router.navigate(['/admin-dashboard']);
-            } else if ( userData.tipo === 'usuario') {
-              this.router.navigate(['/usuario-dashboard']);
-            } else {
-              this.router.navigate(['/invitado-dashboard']);
-            }
-          }
-        } catch (error) {
-          this.router.navigate(['login']);
-        }  
-      } else {
-        this.router.navigate(['login']);
-      }
-    });
-  }
-
-  async checkHuellaDigital() {
-    try {
-      await NativeBiometric.verifyIdentity({
-        reason: 'Por favor, autentícate para continuar',
-        title: 'Autentición Biométrica',
-        subtitle: 'Usa tu huella digítal o Face ID',
-        description: 'Coloca tu huella en el sensor para ingresar.'
-      });
-    } catch (error) {
-      throw error; // Forzamos el error para capturarlo
-    }
-  }
-
-
 
 }
